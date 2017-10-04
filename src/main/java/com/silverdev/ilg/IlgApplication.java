3b10dcfdee4693921503cf03f3c08d9bceb271bd
gustavozf <gzf1996@gmail.com>
2017-10-04 00:22:27 -0300

    oilar

---
+++ src/main/java/com/silverdev/ilg/IlgApplication.java
@@ -0,0 +1,12 @@
+package com.silverdev.ilg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlgApplication.class, args);
+	}
+}
---
+++ src/main/java/com/silverdev/ilg/model/Ingressante.java
@@ -0,0 +1,7 @@
+package com.silverdev.ilg.model;
+
+/**
+ * Created by gustavozf on 04/10/17.
+ */
+public class Ingressante {
+}
