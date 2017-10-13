e78a17542982b4c9a1416674ed589a2e4f400353
narcizo <narcizo.gabriel2@gmail.com>
2017-10-13 19:13:36 -0300

    Continuaçao Login

--- src/main/java/com/silverdev/ilg/repository/UsuarioRepository.java
+++ src/main/java/com/silverdev/ilg/repository/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package com.silverdev.ilg.repository;
 
+import com.silverdev.ilg.model.Usuario;
+
+import java.util.Optional;
+
 public interface UsuarioRepository {
+    Optional<Usuario> findByUsername(String username);
 }
--- src/main/java/com/silverdev/ilg/security/ILGUserDetailService.java
+++ src/main/java/com/silverdev/ilg/security/ILGUserDetailService.java
@@ -1,4 +1,28 @@
 package com.silverdev.ilg.security;
 
-public class ILGUserDetailService {
+
+import com.silverdev.ilg.model.Usuario;
+import com.silverdev.ilg.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class ILGUserDetailService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public ILGUserDetailService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByUsername(username);
+        Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Username e/ou password Incorreto"));
+        return new User(username, usuario.getPassword(), getPermissao(usuario));
+    }
 }
