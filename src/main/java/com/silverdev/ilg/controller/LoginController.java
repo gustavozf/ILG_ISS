da7c2aabd1503baab647514ed5cf936b73161d6c
narcizo <narcizo.gabriel2@gmail.com>
2017-10-14 23:05:07 -0300

    omgsosorry Continuaçao Login

--- src/main/java/com/silverdev/ilg/controller/LoginController.java
+++ src/main/java/com/silverdev/ilg/controller/LoginController.java
@@ -1,4 +1,15 @@
 package com.silverdev.ilg.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping
+    public String abreTelaDeLogin(){
+        return "login";
+    }
 }
--- src/main/java/com/silverdev/ilg/security/ILGUserDetailService.java
+++ src/main/java/com/silverdev/ilg/security/ILGUserDetailService.java
@@ -4,12 +4,17 @@ package com.silverdev.ilg.security;
 import com.silverdev.ilg.model.Usuario;
 import com.silverdev.ilg.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 public class ILGUserDetailService implements UserDetailsService {
     private final UsuarioRepository usuarioRepository;
@@ -24,5 +29,11 @@ public class ILGUserDetailService implements UserDetailsService {
         Optional<Usuario> usuarioOptional = usuarioRepository.findByUsername(username);
         Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Username e/ou password Incorreto"));
         return new User(username, usuario.getPassword(), getPermissao(usuario));
+    }
+
+    private Collections<? extends GrantedAuthority> getPermissao(Usuario usuario){
+        Set<SimpleGrantedAuthority> authority = new HashSet<>();
+        authority.add(new SimpleGrantedAuthority(usuario.getRole().toString().topUpperCase()));
+        return authority;
     }
 }
