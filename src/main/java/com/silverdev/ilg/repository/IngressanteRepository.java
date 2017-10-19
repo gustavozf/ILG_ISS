ce22eeb5b9341cac304f59eea76c9b0881a2b138
williamsakata <williamsakata2@gmail.com>
2017-10-19 16:55:14 -0200

    basico do funcionario ainda falta a media

---
+++ src/main/java/com/silverdev/ilg/repository/IngressanteRepository.java
@@ -0,0 +1,7 @@
+package com.silverdev.ilg.repository;
+
+import com.silverdev.ilg.model.Ingressante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngressanteRepository extends JpaRepository<Ingressante,Integer> {
+}
