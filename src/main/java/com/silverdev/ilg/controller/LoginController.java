0872149f6a1a16222fe29116f07523e52f1e937a
narcizo <narcizo.gabriel2@gmail.com>
2017-10-19 13:44:08 -0200

    No changes made

--- src/main/java/com/silverdev/ilg/controller/LoginController.java
+++ src/main/java/com/silverdev/ilg/controller/LoginController.java
@@ -5,9 +5,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping
+    public String acesso(){
+        return "/login/login";
+    }
 
     @GetMapping("/redirectTipoUsuario")
     public String redirecionaUsuario(){
