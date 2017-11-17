07fd2ec597e1ddeb46809ed463ba81a02d867b5a
marianasoder <sodermariana@gmail.com>
2017-11-17 21:47:50 -0200

    habilita inscricao - admin

---
+++ src/main/java/com/silverdev/ilg/controller/AdminHabilitaController.java
@@ -0,0 +1,9 @@
+package com.silverdev.ilg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/habilitaInscricao")
+public class AdminHabilitaController {
+}
