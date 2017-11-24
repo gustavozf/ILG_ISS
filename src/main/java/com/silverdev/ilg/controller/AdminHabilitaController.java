78eb57f6925b6127a4322bb53bd0a0dd23056c16
marianasoder <sodermariana@gmail.com>
2017-11-24 21:19:13 -0200

    testes

--- src/main/java/com/silverdev/ilg/controller/AdminHabilitaController.java
+++ src/main/java/com/silverdev/ilg/controller/AdminHabilitaController.java
@@ -1,9 +1,36 @@
 package com.silverdev.ilg.controller;
 
+import com.silverdev.ilg.model.Inscricao;
+import com.silverdev.ilg.repository.InscricaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/habilitaInscricao")
 public class AdminHabilitaController {
+
+    private final InscricaoRepository inscricaoRepository;
+
+    @Autowired
+    public AdminHabilitaController(InscricaoRepository inscricaoRepository){
+        this.inscricaoRepository = inscricaoRepository;
+    }
+
+    @GetMapping
+    public String habila(Model model){
+
+        List<Inscricao> inscricoes = inscricaoRepository.findAll();
+
+        model.addAttribute("inscricao", inscricoes);
+
+        return "/admin/habilitacao";
+    }
+
+
+
 }
--- src/main/java/com/silverdev/ilg/model/Boleto.java
+++ src/main/java/com/silverdev/ilg/model/Boleto.java
@@ -9,9 +9,59 @@ public class Boleto {
     @Column(name = "boleto_id")
     private Integer id;
 
-    @Column(name = "boleto_vencimento")
-    private String data_vencimento;
+    @Column(name = "boleto_numero")
+    private Integer numero;
+
+    @Column(name = "boleto_caminho")
+    private String caminho;
 
     @Column(name = "boleto_valor")
     private Integer valor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Boleto boleto = (Boleto) o;
+
+        return id != null ? id.equals(boleto.id) : boleto.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCaminho() {
+        return caminho;
+    }
+
+    public void setCaminho(String caminho) {
+        this.caminho = caminho;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
 }
